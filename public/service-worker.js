d575e51af366a6a47626b8ff9f40f0f655e18e84
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-06 14:53:06 -0600

    updated styles.css path in files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/index.html",
     "/api/transaction",
-    "/assets/css/style.css",
+    "/styles.css",
     "/manifest.json",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
