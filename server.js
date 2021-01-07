5cd30d2b7cb584ff9ab7b5a0662af3292a168552
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-06 21:48:43 -0600

    deploying

--- server.js
+++ server.js
@@ -15,14 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
